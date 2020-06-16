67965c421046d8aef16e3d7039d8991a85a3cf85
cvr <almair@gmail.com>
2020-06-16 19:18:40 -0400

    init

---
+++ scraping.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 17:41:10 2020
+
+@author: almair
+"""
+
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium import webdriver
+import time
+import pandas as pd
+
+
